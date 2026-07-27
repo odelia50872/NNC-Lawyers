536717b60232055b804f384c3ebabd2ae33c438d
Odelia Nakash <odelia50872@gmail.com>
2026-07-27 22:37:01 +0300

    Force deploy latest db fix

--- server/tools/db.js
+++ server/tools/db.js
@@ -1,17 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool(
-    process.env.MYSQL_PUBLIC_URL || process.env.DATABASE_URL || {
-        host: process.env.MYSQLHOST || 'localhost',
-        port: process.env.MYSQLPORT || 3306,
-        user: process.env.MYSQLUSER || 'root',
-        password: process.env.MYSQLPASSWORD,
-        database: process.env.MYSQLDATABASE || 'railway',
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
-    }
-);
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST || 'localhost',
+    port: process.env.MYSQLPORT || 3306,
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE || 'railway',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: process.env.MYSQL_PUBLIC_URL ? { rejectUnauthorized: false } : false
+});
 
 module.exports = pool.promise();
