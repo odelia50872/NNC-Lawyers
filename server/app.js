2f94b351adf67afdccac29d18fabeda279a28255
Odelia Nakash <odelia50872@gmail.com>
2026-08-14 15:39:42 +0300

    Disable crossOriginResourcePolicy to allow file access from Vercel

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: false }));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/uploads', express.static('/app/uploads'));
