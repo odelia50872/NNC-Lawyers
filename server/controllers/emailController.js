c6e934f012db6827eb1c3ce7715ae98f875b0b7a
Odelia Nakash <odelia50872@gmail.com>
2026-08-14 17:01:07 +0300

    Switch to Resend HTTP API for email sending

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,16 +1,16 @@
-const transporter = require('../tools/mailer');
+const resend = require('../tools/mailer');
 const { contactEmailContent, welcomeAddedEmailContent } = require('../templates/emailTemplates');
 
 const sendContactEmail = async (req, res) => {
     const { name, email, phone, message, lang } = req.body;
     const { subject, text } = (contactEmailContent[lang] || contactEmailContent.he)(name, email, phone, message);
     try {
-        await transporter.sendMail({
-            from: `"NNC-Law" <onboarding@resend.dev>`,
+        await resend.emails.send({
+            from: 'NNC-Law <onboarding@resend.dev>',
             to: process.env.EMAIL_USER,
             subject,
             text,
-            replyTo: email,
+            reply_to: email,
         });
         res.json({ success: true });
     } catch (err) {
@@ -23,8 +23,8 @@ const sendWelcomeEmail = async (req, res) => {
     const { name, email, password, lang } = req.body;
     const { subject, html } = (welcomeAddedEmailContent[lang] || welcomeAddedEmailContent.he)(name, email, password);
     try {
-        await transporter.sendMail({
-            from: `"NNC-Law" <onboarding@resend.dev>`,
+        await resend.emails.send({
+            from: 'NNC-Law <onboarding@resend.dev>',
             to: email,
             subject,
             html,
--- server/tools/mailer.js
+++ server/tools/mailer.js
@@ -1,12 +1,3 @@
-const nodemailer = require('nodemailer');
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-
-const transporter = nodemailer.createTransport({
-    host: 'smtp.resend.com',
-    port: 465,
-    secure: true,
-    auth: { user: 'resend', pass: process.env.RESEND_API_KEY },
-});
-
-module.exports = transporter;
+const { Resend } = require('resend');
+const resend = new Resend(process.env.RESEND_API_KEY);
+module.exports = resend;
